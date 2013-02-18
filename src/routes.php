d5337b2e60218f9588429498834ba3c357cad2dc
Doug Grubba <doug@theideapeople.com>
2013-02-17 23:45:18 -0500

    setup some default routes

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,33 @@
 <?php
 
-Route::get('/', function()
-{
-	return View::make('hello');
+Route::get('login', function() {
+	return 'login';
+});
+
+Route::post('login', function() {
+	//@todo try to login the user
+
+	return Redirect::to('home');
+});
+
+Route::get('logout', function() {
+
+	//@todo log the user out
+
+	return Redirect::to('login');
+});
+
+Route::get('register', function() {
+	return 'register';
+});
+
+Route::get('forgot', function() {
+	return 'forgot';
+});
+
+Route::post('forgot', function() {
+
+	//@todo try to resend the user info
+
+	return Redirect::to('login');
 });
